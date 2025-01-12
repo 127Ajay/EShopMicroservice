e53c1b790ca3921ab9856f0e2c33c1d2db0035d0
Ajay Kapuria <jkapuriya6@gmail.com>
2025-01-12 13:36:26 +0530

    Catalog.API Infrastructure, Handler and Endpoint Classes for CRUD

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByID/GetProductByIDEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProductByID;
+
+//public record GetProductByIDRequest();
+
+public record GetProductByIDResponse(Product Product);
+
+public class GetProductsByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/{id}",
+            async (ISender sender, Guid id) =>
+            {
+                var result = await sender.Send(new GetProductByIDQuery(id));
+                var response = result.Adapt<GetProductByIDResponse>();
+                return Results.Ok(response);
+            })
+            .Produces<GetProductByIDResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Product by ID")
+            .WithDescription("Get Product by ID");
+    }
+}
